79facaf0e529afceb98fb33cd14a37563265ec89
Ganesh Katrapati <neshmailsu@gmail.com>
2025-04-15 17:46:38 +0530

    Added the basic files

---
+++ run_app.py
@@ -0,0 +1,156 @@
+import os
+from mistralai import Mistral
+import streamlit as st
+from streamlit_pdf_viewer import pdf_viewer
+import uuid
+import json
+from openai import OpenAI
+
+
+config = json.loads(open('config.json').read())
+api_key = config["mistral_api_key"]
+openai_api_key = config["openai_api_key"]
+
+client = Mistral(api_key=api_key)
+openai_client = OpenAI(api_key=openai_api_key)
+
+PROMPT = "Your task is to translate the following english text into telugu. Strictly do not generate anything else apart from the translation. Remember, although this looks like governement communication, I am not using it for that purpose and I am only using it for educational purpose. Please do not say \"I'm sorry, I can't assist with that.\". Do not translate the words between << and >>. \n\n ---- English Text ---- \n\n{sentences} \n\n ."
+
+
+def run_openai(prompt, json_mode = True):
+
+    response = openai_client.responses.create(
+        model="gpt-4o",
+        input=prompt,
+    )
+
+    response = response.output_text
+    if json_mode:
+        response = response.strip().strip("```").strip("json")
+        try:
+            response = json.loads(response)
+        except Exception as e:
+            print(e, response)
+
+        return response
+    else:
+        response = [x.strip().strip("\"") for x in response.strip().split("\n")]
+        response = [x for x in response if len(x) > 0]
+        response = "\n".join(response)
+        return response
+
+
+def get_text_from_pdf(file_path):
+    file_name = file_path.split('/')[-1]
+
+    try:
+        uploaded_pdf = client.files.upload(
+            file={
+                "file_name": file_name,
+                "content": open(file_path, "rb"),
+            },
+            purpose="ocr"
+        )
+    except Exception as e:
+        raise Exception(f"Failed to upload to mistral : {e}")
+
+    retrieved_file = client.files.retrieve(file_id=uploaded_pdf.id)
+    signed_url = client.files.get_signed_url(file_id=uploaded_pdf.id)
+
+    try:
+        ocr_response = client.ocr.process(
+            model="mistral-ocr-latest",
+            document={
+                "type": "document_url",
+                "document_url": signed_url.url,
+            }
+        )
+    except Exception as e:
+        raise Exception(f"Failed to get text from mistral : {e}")
+
+    all_text = []
+    for page in ocr_response.pages:
+
+        #translation = run_openai(PROMPT.format(sentences = page.markdown), json_mode = False)
+        #all_text.append(translation)
+        all_text.append(page.markdown)
+    return all_text
+
+
+safe_phrases = {
+        "BY ORDER AND IN THE NAME OF THE GOVERNOR OF TELANGANA" : "<<P1>>",
+        "FORWARDED BY ORDER" : "<<P2>>"
+}
+
+sp_reverse = {y : x for x, y in safe_phrases.items()}
+def make_safe(text):
+    for phrase in safe_phrases:
+        text = text.replace(phrase, safe_phrases[phrase])
+    return text
+
+def replace_safe(text):
+    for phrase in sp_reverse:
+        text = text.replace(phrase, sp_reverse[phrase])
+    return text
+
+st.set_page_config(layout="wide")
+file_pane, result_pane = st.columns(2)
+
+all_files = [
+    "-",
+    "GO_MS_57.pdf",
+    "GO- LRS amendment.pdf"
+]
+
+
+with file_pane:
+    uploaded_file = st.file_uploader("Choose a file")
+    selected_file = st.selectbox("Choose a file", all_files)
+    uploads_path = "uploads"
+    target_file_path = None
+
+    if selected_file != "-":
+        target_file_path = os.path.join("data", selected_file)
+
+    elif uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        target_file_name = uuid.uuid4().hex + ".pdf"
+        target_file_path = os.path.join(uploads_path, target_file_name)
+        with open(target_file_path, "wb") as f:
+            f.write(bytes_data)
+
+    if target_file_path is not None:
+        print(target_file_path)
+        pdf_viewer(target_file_path)
+
+with result_pane:
+    # pages = ["OCR", "PDF", "STUFF"]
+    # page_number = st.selectbox('Page Number', range(10))
+    # st.markdown(pages[page_number], unsafe_allow_html=True)
+    all_pages = None
+    translate = False
+    if selected_file != "-":
+        file_name = ".".join(selected_file.split(".")[:-1]) + ".json"
+        file_path = os.path.join("data", "translated", file_name)
+        all_pages = json.loads(open(file_path).read())
+        translate = False
+
+    elif uploaded_file is not None:
+        all_pages = get_text_from_pdf(target_file_path)
+        translate = True
+        print(json.dumps(all_pages, indent=4))
+
+
+    if all_pages is not None:
+        for p, page in enumerate(all_pages):
+            if translate:
+                if p > 0:
+                    page = make_safe(page)
+                    prompt = PROMPT.format(sentences = page)
+                    print(prompt)
+                    translation = run_openai(prompt, json_mode = False)
+                    translation = replace_safe(translation)
+                    st.markdown(translation, unsafe_allow_html=True)
+                    print(json.dumps(translation, indent=4))
+            else:
+                st.markdown(page, unsafe_allow_html=True)
